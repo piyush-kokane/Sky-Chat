55e8169343085d40f929c2d7a1c46c9b539317e1
Piyush Kokane <pjkokane21@gmail.com>
2025-10-12 00:36:51 +0530

    Landing page

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,11 +1,17 @@
+import icon from "@assets/SkyChat-logo.png"
 import './Landing.css'
 
 
 
 function Landing() {
   return (
-    <div>
+    <div className="landing-pg">
+      <div className="landing-bg" />
 
+      <img src={icon} />
+      <span> <h1>Sky</h1> <h2>Chat</h2> </span>
+
+      <button>Login</button>
     </div>
   );
 }
